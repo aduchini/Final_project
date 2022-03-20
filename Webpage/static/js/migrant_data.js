465e70860219c931822c107814313d4792e982d9
aqua2504 <aakriti2504@gmail.com>
2022-03-20 10:34:20 -0700

    Text analysis

---
+++ Webpage/static/js/migrant_data.js
@@ -0,0 +1,100 @@
+var data  = [{
+  "data_date": "2022-02-24",
+  "unix_timestamp": 1645660800,
+  "individuals": 84681
+  },
+  {
+  "data_date": "2022-02-25",
+  "unix_timestamp": 1645747200,
+  "individuals": 192982
+  },
+  {
+  "data_date": "2022-02-26",
+  "unix_timestamp": 1645833600,
+  "individuals": 341301
+  },
+  {
+  "data_date": "2022-02-27",
+  "unix_timestamp": 1645920000,
+  "individuals": 509665
+  },
+  {
+  "data_date": "2022-02-28",
+  "unix_timestamp": 1646006400,
+  "individuals": 672139
+  },
+  {
+  "data_date": "2022-03-01",
+  "unix_timestamp": 1646092800,
+  "individuals": 838829
+  },
+  {
+  "data_date": "2022-03-02",
+  "unix_timestamp": 1646179200,
+  "individuals": 1033312
+  },
+  {
+  "data_date": "2022-03-03",
+  "unix_timestamp": 1646265600,
+  "individuals": 1201217
+  },
+  {
+  "data_date": "2022-03-04",
+  "unix_timestamp": 1646352000,
+  "individuals": 1373316
+  },
+  {
+  "data_date": "2022-03-05",
+  "unix_timestamp": 1646438400,
+  "individuals": 1570836
+  },
+  {
+  "data_date": "2022-03-06",
+  "unix_timestamp": 1646524800,
+  "individuals": 1779996
+  },
+  {
+  "data_date": "2022-03-07",
+  "unix_timestamp": 1646611200,
+  "individuals": 1987656
+  },
+  {
+  "data_date": "2022-03-08",
+  "unix_timestamp": 1646697600,
+  "individuals": 2172694
+  },
+  {
+  "data_date": "2022-03-09",
+  "unix_timestamp": 1646784000,
+  "individuals": 2343537
+  },
+  {
+  "data_date": "2022-03-10",
+  "unix_timestamp": 1646870400,
+  "individuals": 2478204
+  },
+  {
+  "data_date": "2022-03-11",
+  "unix_timestamp": 1646956800,
+  "individuals": 2597460
+  },
+  {
+  "data_date": "2022-03-12",
+  "unix_timestamp": 1647043200,
+  "individuals": 2713282
+  },
+  {
+  "data_date": "2022-03-13",
+  "unix_timestamp": 1647129600,
+  "individuals": 2838599
+  },
+  {
+  "data_date": "2022-03-14",
+  "unix_timestamp": 1647216000,
+  "individuals": 2952275
+  },
+  {
+  "data_date": "2022-03-15",
+  "unix_timestamp": 1647302400,
+  "individuals": 3000381
+  }];
