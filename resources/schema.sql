578fc3dd10f984ea4967fbb5e99491c35268d98c
Ana D <ana.duchini@gmail.com>
2022-03-26 23:15:13 -0700

    adding Tableau working file that uses SQLite database. updating README with file links. updating schema and workflow chart

--- resources/schema.sql
+++ resources/schema.sql
@@ -1,46 +1,39 @@
 -- THIS IS THE SOURCE DATA
-CREATE TABLE UkraineCombinedTweetsDeduped (
-    username TEXT PRIMARY KEY NOT NULL UNIQUE,
-    acctdesc TEXT,
+CREATE TABLE tweets (
+    usercreatedts DATE NOT NULL,
     location TEXT,
-    following INT,
-    followers INT,
-    totaltweets INT,
-    usercreatedts DATE,
-    tweetid BIGINT,
-    tweetcreatedts DATE,
-    retweetcount INT,	
     text TEXT,
-    hashtags TEXT, 
-    language TEXT,
-    coordinates TEXT,
-    favorite_count INT,
-    extractedts DATE
+    country TEXT,
+    neg FLOAT,
+    neu FLOAT,
+    pos FLOAT,
+    compound FLOAT,
+    sentiment TEXT
 );
--- THIS IS ALL THE CLEANED DATA INCLUDING VADER SENTIMENTS
-CREATE TABLE tweets_data (
-  tweetid BIGINT PRIMARY KEY NOT NULL UNIQUE,
-  username TEXT,
-  retweetcount INT,
-  favorite_count INT,
-  tweetcreatedts DATE,
-  text TEXT,
-  country TEXT,
-  neg FLOAT,
-  neu FLOAT,
-  pos FLOAT,
-  compound FLOAT,
-  cleaned_text TEXT,
-  sentiment TEXT
- -- latitude FLOAT,
- -- longitude FLOAT
+-- THIS IS THE CLEANED DATA
+CREATE TABLE migration (
+  data_date DATE NOT NULL,
+  people INT
 );
 
 -- This table will contain the results of the analysis
-CREATE TABLE RussianPersonnelLosses (
-    date DATE PRIMARY KEY NOT NULL UNIQUE,
-    day INT,
-    personnel INT,
-    personnel* TEXT,
-    POW INT
+CREATE TABLE casualties (
+    Date DATE NOT NULL,
+    casualties INT
 );
+
+CREATE TABLE migration_sentiments (
+  data_date DATE NOT NULL, 
+  people FLOAT, 
+  casualties INT, 
+  compound_sentiment FLOAT
+  );
+
+  INSERT INTO migration_sentiments 
+    SELECT mg.data_date, mg.people, ca.casualties, AVG(tw.compound)
+        FROM migration as mg
+        LEFT JOIN casualties as ca
+            ON mg.data_date = ca.Date
+        LEFT JOIN tweets as tw
+            ON tw.tweetcreatedts = mg.data_date
+        GROUP BY mg.data_date;
